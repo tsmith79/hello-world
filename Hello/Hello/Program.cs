40d37020d933e35dc1774da83ed1916d978cfe11
tsmith79 <32547245+tsmith79@users.noreply.github.com>
2017-10-06 11:52:09 -0400

    Add files via upload

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Your Name:");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            
+
+            if (hoursOfSleep > 8)
+            {
+                Console.WriteLine("You are well rested!");
+            
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep.");
+            }
+        }
+    }
+}
